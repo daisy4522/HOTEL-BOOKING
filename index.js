b8f7f9ece7a6506aef876e45f82fad492e2334e8
daisy4522 <jepletingdaisy78@gmail.com>
2025-04-30 11:00:08 +0300

    lobo system project

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
+
+const dbConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "lobo sys_db",
+});
+
+const app = express();
+
+// middleware
+app.use(express.static(path.join(__dirname, "public"))); // path -- nested routs /booking/user/id
+app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded - form data
+
+// routes
+
+app.get("/", (req, res) => {
+  dbConnection.query("SELECT * FROM rooms", (roomsSelectError, rooms) => {
+    if (roomsSelectError) {
+      res.status(500).send("Server Error: 500");
+    } else {
+      dbConnection.query("SELECT * FROM spots", (spotsSelectError, spots) => {
+        if (spotsSelectError) {
+          res.status(500).send("Server Error: 500");
+        } else {
+          console.log(spots);
+          res.render("index.ejs", {rooms, spots});
+        }
+      });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
